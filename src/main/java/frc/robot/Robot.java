7007134a3bfc8c7d00ec3f34fff32ed239787e9b
kpratap1 <121640740+kpratap1@users.noreply.github.com>
2025-01-06 21:50:16 -0800

    Initial commit

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,189 @@
+package frc.robot;
+
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class Robot extends TimedRobot {
+  private final XboxController driver = new XboxController(0); // Initializes the driver controller.
+  // private final XboxController operator = new XboxController(1); // Initializes the operator controller.
+
+  // Limits the acceleration of the drivetrain by smoothing controller inputs.
+  private final SlewRateLimiter xAccLimiter = new SlewRateLimiter(Drivetrain.maxAccTeleop / Drivetrain.maxVelTeleop);
+  private final SlewRateLimiter yAccLimiter = new SlewRateLimiter(Drivetrain.maxAccTeleop / Drivetrain.maxVelTeleop);
+  private final SlewRateLimiter angAccLimiter = new SlewRateLimiter(Drivetrain.maxAngAccTeleop / Drivetrain.maxAngVelTeleop);
+
+  private double speedScaleFactor = 1.0; // Scales the speed of the robot that results from controller inputs. 1.0 corresponds to full speed. 0.0 is fully stopped.
+  private boolean lock = false; // Controls whether the swerve drive is in x-lock (for defense) or is driving. 
+
+  // Initializes the different subsystems of the robot.
+  private final Drivetrain swerve = new Drivetrain(); // Contains the Swerve Modules, Gyro, Path Follower, Target Tracking, Odometry, and Vision Calibration.
+
+  // Auto Variables
+  private final SendableChooser<String> autoChooser = new SendableChooser<>();
+  private static final String auto1 = "auto1";
+  private static final String auto2 = "auto2";
+  private String autoSelected;
+  private int autoStage = 1;
+
+  public void robotInit() {
+    // Configures the auto chooser on the dashboard.
+    autoChooser.setDefaultOption(auto1, auto1);
+    autoChooser.addOption(auto2, auto2);
+    SmartDashboard.putData("Autos", autoChooser);
+
+    swerve.loadPath("Example Path", 0.0, 0.0, 0.0, 180.0); // Loads a Path Planner generated path into the path follower code in the drivetrain. 
+    runAll(); // Helps prevent loop overruns on startup by running every command before the match starts.
+  }
+
+  public void robotPeriodic() {
+    // Publishes information about the robot and robot subsystems to the Dashboard.
+    swerve.updateDash();
+    updateDash();
+
+    swerve.updateVisionHeading(); // Updates the Limelights with the robot heading (for MegaTag2).
+    if (driver.getRawButtonPressed(8)) swerve.resetGyro(); // Menu Button re-zeros the angle reading of the gyro to the current angle of the robot. Should be called if the gyroscope readings are no longer well correlated with the field.
+  }
+
+  public void autonomousInit() {
+    swerve.pushCalibration(); // Updates the robot's position on the field.
+    autoStage = 1;
+    autoSelected = autoChooser.getSelected();
+    switch (autoSelected) {
+      case auto1:
+        // AutoInit 1 code goes here.
+      break;
+
+      case auto2:
+        // AutoInit 2 code goes here.
+      break;
+    }
+  }
+
+  public void autonomousPeriodic() {
+    swerve.updateOdometry(); // Keeps track of the position of the robot on the field. Must be called each period.
+    switch (autoSelected) {
+      case auto1:
+        switch (autoStage) {
+          case 1:
+            // Auto 1, Stage 1 code goes here.
+          break;
+
+          case 2:
+            // Auto 1, Stage 2 code goes here.
+          break;
+        }
+      break;
+
+      case auto2:
+        switch (autoStage) {
+          case 1:
+            // Auto 2, Stage 1 code goes here.
+          break;
+
+          case 2:
+            // Auto 2, Stage 2 code goes here.
+          break;
+        }
+      break;
+    }
+  }
+  
+  public void teleopInit() {
+    swerve.pushCalibration(); // Updates the robot's position on the field.
+  }
+
+  public void teleopPeriodic() {
+    swerve.updateOdometry(); // Keeps track of the position of the robot on the field. Must be called each period.
+    for (int limelightIndex = 0; limelightIndex < swerve.limelights.length; limelightIndex++) { // Iterates through each limelight.
+      swerve.addVisionEstimate(limelightIndex, 0.7, 0.7, Units.degreesToRadians(Math.pow(10, 10)), true); // Checks to see ifs there are reliable April Tags in sight of the Limelight and updates the robot position on the field.
+    }
+
+    if (driver.getRawButtonPressed(4)) speedScaleFactor = 1.0; // Y Button sets the drivetrain in full speed mode.
+    if (driver.getRawButtonPressed(2)) speedScaleFactor = 0.6; // B button sets the drivetrain in medium speed mode.
+    if (driver.getRawButtonPressed(1)) speedScaleFactor = 0.15; // A button sets the drivetrain in low speed mode.
+
+    // Applies a deadband to controller inputs. Also limits the acceleration of controller inputs.
+    double xVel = xAccLimiter.calculate(MathUtil.applyDeadband(-driver.getLeftY(), 0.05)*speedScaleFactor)*Drivetrain.maxVelTeleop;
+    double yVel = yAccLimiter.calculate(MathUtil.applyDeadband(-driver.getLeftX(), 0.05)*speedScaleFactor)*Drivetrain.maxVelTeleop;
+    double angVel = angAccLimiter.calculate(MathUtil.applyDeadband(-driver.getRightX(), 0.05)*speedScaleFactor)*Drivetrain.maxAngVelTeleop;
+
+    if (driver.getRawButton(3)) {
+      lock = true; // Pressing the X-button causes the swerve modules to lock (for defense).
+    } else if (Math.abs(driver.getLeftY()) >= 0.05 || Math.abs(driver.getLeftX()) >= 0.05 || Math.abs(driver.getRightX()) >= 0.05) {
+      lock = false; // Pressing any joystick more than 5% will cause the swerve modules stop locking and begin driving.
+    }
+
+    if (lock) {
+      swerve.xLock(); // Locks the swerve modules (for defense).
+    } else {
+      swerve.drive(xVel, yVel, angVel, true, 0.0, 0.0); // Drive at the velocity demanded by the controller.
+    }
+
+    // The following 3 calls allow the user to calibrate the position of the robot based on April Tag information. Should be called when the robot is stationary. Button 7 is "View", the right center button.
+    if (driver.getRawButtonPressed(7)) swerve.resetCalibration(); // Begins calculating the position of the robot on the field based on visible April Tags.
+    if (driver.getRawButton(7)) {
+      for (int limelightIndex = 0; limelightIndex < swerve.limelights.length; limelightIndex++) { // Iterates through each limelight.
+        swerve.addCalibrationEstimate(limelightIndex, false); // Collects additional data to calculate the position of the robot on the field based on visible April Tags.
+      }
+    }
+    if (driver.getRawButtonReleased(7)) swerve.pushCalibration(); // Updates the position of the robot on the field based on previous calculations.
+  }
+
+  public void disabledInit() {    
+    swerve.resetCalibration(); // Begins calculating the position of the robot on the field based on visible April Tags.
+  }
+
+  public void disabledPeriodic() {
+    swerve.updateOdometry(); // Keeps track of the position of the robot on the field. Must be called each period.
+    for (int limelightIndex = 0; limelightIndex < swerve.limelights.length; limelightIndex++) { // Iterates through each limelight.
+      swerve.addCalibrationEstimate(limelightIndex, false); // Collects additional data to calculate the position of the robot on the field based on visible April Tags.
+    }
+  }
+
+  // Publishes information to the dashboard.
+  public void updateDash() {
+    //SmartDashboard.putNumber("Speed Scale Factor", speedScaleFactor);
+    //SmartDashboard.putNumber("Auto Stage", autoStage);
+  }
+
+  // Helps prevent loop overruns on startup by running every user created command in every class before the match starts. Not sure why this helps, but it does.
+  public void runAll() { 
+    swerve.resetDriveController(0.0);
+    swerve.xLock();
+    swerve.aimDrive(-3.0, 2.0, 105.0, false);
+    swerve.driveTo(1.0, -2.0, -75.0);
+    swerve.resetPathController(0);
+    swerve.followPath(0);
+    swerve.pushCalibration();
+    swerve.addCalibrationEstimate(0, false);
+    swerve.pushCalibration();
+    swerve.resetCalibration();
+    swerve.resetGyro();
+    swerve.updateVisionHeading();
+    swerve.addVisionEstimate(0, 0.7, 0.7, Units.degreesToRadians(Math.pow(10, 10)), true);
+    swerve.updateOdometry();
+    swerve.drive(0.01, 0.0, 0.0, true, 0.0, 0.0);
+    System.out.println("swerve atDriveGoal: " + swerve.atDriveGoal());
+    System.out.println("swerve atPathEndpoint: " + swerve.atPathEndpoint(0));
+    System.out.println("swerve getAngVel: " + swerve.getAngVel());
+    System.out.println("swerve getCalibrationTimer: " + swerve.getCalibrationTimer());
+    System.out.println("swerve getFusedAng: " + swerve.getFusedAng());
+    System.out.println("swerve getGyroAng: " + swerve.getGyroAng());
+    System.out.println("swerve getGyroPitch: " + swerve.getGyroPitch());
+    System.out.println("swerve getPathAngleError: " + swerve.getPathAngleError());
+    System.out.println("swerve getPathPosError: " + swerve.getPathPosError());
+    System.out.println("swerve getXPos: " + swerve.getXPos());
+    System.out.println("swerve getXVel: " + swerve.getXVel());
+    System.out.println("swerve getYPos: " + swerve.getYPos());
+    System.out.println("swerve getYVel: " + swerve.getYVel());
+    System.out.println("swerve isBlueAlliance: " + swerve.isBlueAlliance());
+    System.out.println("swerve isRedAlliance: " + swerve.isRedAlliance());
+    swerve.updateDash();
+    updateDash();
+  }
+}
---
+++ src/main/java/frc/robot/SwerveModule.java
@@ -0,0 +1,134 @@
+package frc.robot;
+
+import com.ctre.phoenix6.BaseStatusSignal;
+import com.ctre.phoenix6.configs.CANcoderConfiguration;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.CANcoder;
+import com.ctre.phoenix6.hardware.ParentDevice;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.signals.SensorDirectionValue;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+class SwerveModule {
+  public static final double correctionFactor = 0.97; // Factor that corrects for real-world deviations from the odometry calculated position of the robot. These can be caused by things like tread wear. Set this value to 1, then make the robot follow a 1 meter path in auto. Set this value to the distance the robot actually traveled.
+  public static final double wheelCirc = 4.0*0.0254*Math.PI; // Circumference of the wheel. Unit: meters
+  public static final double turnGearRatio = 150.0/7.0; // Turn motor rotor rotations per turn rotation of the swerve wheel.
+  public static final double driveGearRatio = 300.0/49.0; // Drive motor rotor rotations per drive rotation of the swerve wheel.
+  public static final double maxVel = 5800.0*wheelCirc*correctionFactor/(60.0*driveGearRatio); // The maximum speed of the swerve module in meters per second.
+  public final CANcoder wheelEncoder; // The CANcoder that measures the angle of the swerve wheel.
+  public final TalonFX driveMotor; // The Kraken X60 motor that controls the driving of the swerve module.
+  public final TalonFX turnMotor; // The Kraken X60 motor that controls the turning of the swerve module.
+
+  public SwerveModule(int turnID, int driveID, int encoderID, boolean invertDrive, double wheelEncoderZero, String canbus) {
+    wheelEncoder = new CANcoder(encoderID, canbus);
+    configEncoder(wheelEncoder, wheelEncoderZero);
+    turnMotor = new TalonFX(turnID, canbus);
+    configTurnMotor(turnMotor, true, 40.0);
+    driveMotor = new TalonFX(driveID, canbus);
+    configDriveMotor(driveMotor, invertDrive, 80.0);
+    driveMotor.setPosition(0.0, 0.03);
+    BaseStatusSignal.setUpdateFrequencyForAll(250.0, driveMotor.getPosition(), driveMotor.getVelocity(), wheelEncoder.getAbsolutePosition(), wheelEncoder.getVelocity(), turnMotor.getDutyCycle());
+    ParentDevice.optimizeBusUtilizationForAll(driveMotor, turnMotor, wheelEncoder);
+  }
+
+  // Sets the swerve module to the given state (velocity and angle).
+  public void setSMS(SwerveModuleState desiredState) {
+    Rotation2d currentWheelAngle = Rotation2d.fromDegrees(getWheelAngle());
+    desiredState.optimize(currentWheelAngle); // Minimizes the amount a wheel needs to rotate by inverting the direction of the drive motor in some situations. 
+    setAngle(desiredState.angle.getDegrees());
+    setVel(desiredState.speedMetersPerSecond*desiredState.angle.minus(currentWheelAngle).getCos()); // Cosine compensation. If a wheel is not at its angular setpoint, its velocity setpoint is reduced.
+  }
+  
+  // Returns the postion and angle of the module.
+  public SwerveModulePosition getSMP() {
+    return new SwerveModulePosition(getDriveMotorPos(), Rotation2d.fromDegrees(getWheelAngle()));
+  }
+
+  // Returns total distance the wheel has rotated. Unit: meters
+  public double getDriveMotorPos() {
+    return BaseStatusSignal.getLatencyCompensatedValueAsDouble(driveMotor.getPosition(), driveMotor.getVelocity(), 0.02)*wheelCirc*correctionFactor/driveGearRatio;
+  }
+  
+  // Returns the raw value of the wheel encoder. Range: -180 to 180 degrees. 0 degrees corresponds to facing to the front (+x). 90 degrees in facing left (+y). CCW positive coordinate system.
+  public double getWheelAngle() {
+    return BaseStatusSignal.getLatencyCompensatedValueAsDouble(wheelEncoder.getAbsolutePosition(), wheelEncoder.getVelocity(), 0.02)*360.0;
+  }
+  
+  // Sets the velocity of the module. Units: meters per second
+  private void setVel(double vel) {
+    driveMotor.setControl(new VelocityVoltage(vel*driveGearRatio/(wheelCirc*correctionFactor)).withEnableFOC(true));
+  }
+  
+  // Sets the angle of the module. Units: degrees Can accept values outside of -180 to 180, corresponding to multiple rotations of the swerve wheel.
+  private void setAngle(double angle) {
+    turnMotor.setControl(new MotionMagicTorqueCurrentFOC(angle/360.0));
+  }
+
+  // Configures the swerve module's drive motor.
+  private void configDriveMotor(TalonFX motor, boolean invert, double currentLimit) {
+    TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
+
+    motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    motorConfigs.MotorOutput.Inverted = invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+
+    // Current limit configuration.
+    motorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+    motorConfigs.CurrentLimits.StatorCurrentLimit = currentLimit;
+
+    // VelocityVoltage closed-loop control configuration.
+    motorConfigs.Slot0.kP = 0.25; // Units: volts per 1 motor rotation per second of error.
+    motorConfigs.Slot0.kI = 0.5; // Units: volts per 1 motor rotation per second * 1 second of error.
+    motorConfigs.Slot0.kD = 0.0; // Units: volts per 1 motor rotation per second / 1 second of error.
+    motorConfigs.Slot0.kV = 0.12; // The amount of voltage required to create 1 motor rotation per second.
+    motorConfigs.Slot0.kS = 0.16; // The amount of voltage required to barely overcome static friction in the swerve wheel.
+
+    motor.getConfigurator().apply(motorConfigs, 0.03);
+  }
+
+  // Configures the swerve module's turn motor.
+  private void configTurnMotor(TalonFX motor, boolean invert, double currentLimit) {
+    TalonFXConfiguration motorConfigs = new TalonFXConfiguration();
+
+    motorConfigs.MotorOutput.NeutralMode = NeutralModeValue.Brake;
+    motorConfigs.MotorOutput.Inverted = invert ? InvertedValue.Clockwise_Positive : InvertedValue.CounterClockwise_Positive;
+
+    // Current limit configuration.
+    motorConfigs.CurrentLimits.StatorCurrentLimitEnable = true;
+    motorConfigs.CurrentLimits.StatorCurrentLimit = currentLimit;
+
+    // MotionMagicTorqueFOC closed-loop control configuration.
+    motorConfigs.Slot0.kP = 800.0; // Units: amperes per 1 swerve wheel rotation of error.
+    motorConfigs.Slot0.kI = 0.0; // Units: amperes per 1 swerve wheel rotation * 1 second of error.
+    motorConfigs.Slot0.kD = 18.0; // Units: amperes per 1 swerve wheel rotation / 1 second of error.
+    motorConfigs.MotionMagic.MotionMagicAcceleration = 1000.0/turnGearRatio; // Units: rotations per second per second.
+    motorConfigs.MotionMagic.MotionMagicCruiseVelocity = 100.0/turnGearRatio; // Units: roations per second.
+
+    // CANcoder feedback configurations.
+    motorConfigs.Feedback.FeedbackRemoteSensorID = wheelEncoder.getDeviceID();
+    motorConfigs.Feedback.FeedbackSensorSource = FeedbackSensorSourceValue.FusedCANcoder;
+    motorConfigs.Feedback.SensorToMechanismRatio = 1.0;
+    motorConfigs.Feedback.RotorToSensorRatio = turnGearRatio;
+    motorConfigs.ClosedLoopGeneral.ContinuousWrap = true;
+
+    motor.getConfigurator().apply(motorConfigs, 0.03);
+  }
+
+  // Configures the swerve module's wheel encoder.
+  private void configEncoder(CANcoder encoder, double wheelEncoderZero) {
+    CANcoderConfiguration encoderConfigs = new CANcoderConfiguration();
+
+    encoderConfigs.MagnetSensor.AbsoluteSensorDiscontinuityPoint = 0.5;
+    encoderConfigs.MagnetSensor.SensorDirection = SensorDirectionValue.CounterClockwise_Positive;
+    encoderConfigs.MagnetSensor.MagnetOffset = wheelEncoderZero;
+
+    encoder.getConfigurator().apply(encoderConfigs, 0.03);
+  }
+}
