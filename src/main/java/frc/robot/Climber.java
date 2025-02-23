a2379936da60128736eed3ab2106b5112fb9ebdb
kpratap1 <121640740+kpratap1@users.noreply.github.com>
2025-02-23 13:18:21 -0800

    Update Climber.java

--- src/main/java/frc/robot/Climber.java
+++ src/main/java/frc/robot/Climber.java
@@ -1,6 +1,5 @@
 package frc.robot;
 
-import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.DutyCycleOut;
 import com.ctre.phoenix6.hardware.ParentDevice;
