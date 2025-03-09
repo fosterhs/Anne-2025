19a982d33afe785a8fd55d12e23621133a38a5c9
kpratap1 <121640740+kpratap1@users.noreply.github.com>
2025-03-08 23:12:51 -0800

    Update AlgaeYeeter.java

--- src/main/java/frc/robot/AlgaeYeeter.java
+++ src/main/java/frc/robot/AlgaeYeeter.java
@@ -7,7 +7,6 @@ import com.ctre.phoenix6.controls.MotionMagicTorqueCurrentFOC;
 import com.ctre.phoenix6.controls.TorqueCurrentFOC;
 import com.ctre.phoenix6.hardware.ParentDevice;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 import com.ctre.phoenix6.signals.GravityTypeValue;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
